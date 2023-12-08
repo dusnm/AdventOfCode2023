2706d988433371809eb0f7ff84dbd14dce7e9835
Dušan Mitrović <dusan@dusanmitrovic.xyz>
2023-12-08 22:10:47 +0100

    Refactor day8 part 1
    Signed-off-by: Dušan Mitrović <dusan@dusanmitrovic.xyz>

--- pkg/day8/puzzle1.go
+++ pkg/day8/puzzle1.go
@@ -1,49 +1,44 @@
 package day8
 
 const (
-	GameStart            = "AAA"
-	GameEnd              = "ZZZ"
-	InstructionLeft rune = 'L'
+	GameStart = "AAA"
+	GameEnd   = "ZZZ"
 )
 
 func Puzzle1(input string) int {
 	game := New(input)
 
-	return Walk(&game, GameStart, 0, false, 0)
+	return Walk(&game)
 }
 
-func Walk(
-	game *Game,
-	lookup string,
-	nextInstruction int,
-	reachedEnd bool,
-	depth int,
-) int {
-	if reachedEnd {
-		return depth
+func Walk(game *Game) int {
+	depth, next := 0, 0
+	lookup := GameStart
+
+	for {
+		var instruction rune
+		if len(game.Instructions) > next {
+			instruction = game.Instructions[next]
+		} else {
+			instruction = game.Instructions[0]
+			next = 0
+		}
+
+		value := game.Nodes[lookup]
+
+		if value.Value == GameEnd {
+			break
+		}
+
+		if instruction == 'L' {
+			lookup = value.Left
+		} else {
+			lookup = value.Right
+		}
+
+		next++
+		depth++
 	}
 
-	var instruction rune
-
-	if len(game.Instructions) > nextInstruction {
-		instruction = game.Instructions[nextInstruction]
-	} else {
-		instruction = game.Instructions[0]
-		nextInstruction = 0
-	}
-
-	value := game.Nodes[lookup]
-
-	if value.Value == GameEnd {
-		return Walk(game, value.Value, nextInstruction, true, depth)
-	}
-
-	nextInstruction++
-	depth++
-
-	if instruction == InstructionLeft {
-		return Walk(game, value.Left, nextInstruction, false, depth)
-	}
-
-	return Walk(game, value.Right, nextInstruction, false, depth)
+	return depth
 }
